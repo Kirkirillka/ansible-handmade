162c67d5ef246174685731653097f74bf1709abf
Kirill Kukarcev <kirillkukarcev@gmail.com>
2019-04-26 15:11:08 +0300

    OpenVSwitch Ansible Role installation.
    Tested on CentOS7 only.
    OpenVSwitch ver. 2.4.2

---
+++ playbooks/openvswitch/install_openvswitch.sh
@@ -0,0 +1,40 @@
+# https://www.linuxtechi.com/install-use-openvswitch-kvm-centos-7-rhel-7/
+
+
+###
+# About script
+###
+
+# This script will automatically install OpenVSwitch 2.9.2. (instruction taken from https://www.linuxtechi.com/install-use-openvswitch-kvm-centos-7-rhel-7/). It may be not the latest version, but it was proved that the following features worked well on CentOS7:
+# - Simple bridging several interfaces
+# - Netflow collection
+# - SPAN mirroring
+# - ERSPAN mirroring
+
+# Install packages
+yum install wget openssl-devel  python-sphinx gcc make python-devel openssl-devel kernel-devel graphviz kernel-debug-devel autoconf automake rpm-build redhat-rpm-config libtool python-twisted-core python-zope-interface PyQt4 desktop-file-utils libcap-ng-devel groff checkpolicy selinux-policy-devel -y
+
+# Create user to run the command under
+useradd ovs
+su - ovs
+
+# Download and extract sources
+mkdir -p ~/rpmbuild/SOURCES
+wget http://openvswitch.org/releases/openvswitch-2.9.2.tar.gz
+cp openvswitch-2.9.2.tar.gz ~/rpmbuild/SOURCES/
+tar xfz openvswitch-2.9.2.tar.gz
+rpmbuild -bb --nocheck openvswitch-2.9.2/rhel/openvswitch-fedora.spec
+exit
+
+# Install from RPM package
+yum localinstall /home/ovs/rpmbuild/RPMS/x86_64/openvswitch-2.9.2-1.el7.x86_64.rpm -y
+
+# Run service
+systemctl start openvswitch.service
+systemctl enable openvswitch.service
+
+# Create first bridge
+ovs-vsctl add-br ovs-br0
+
+# At this step, you may add as many host interfaces as you want
+# The bridge ovs-br0 seems as another interface in your machine, so you can use NetworkManager to automatically boot it and set up (via /etc/sysconfig/network-scripts/ifcfg-ovs-vr0). It was tested against DHCP server to sucessfully lease an IP address and to be able to communicate over network.
---
+++ playbooks/openvswitch/links.md
@@ -0,0 +1,39 @@
+
+
+### Linux capabilities
+- https://www.hippolab.ru/virtualnyy-setevoy-interfeys-v-linux-tap-vs-tun
+- https://adamkuj.net/blog/2016/05/18/pro-tip-port-mirroring-in-linux/
+
+
+### What is SPAN/RSPAN/ERSPAN
+- https://medium.com/@k.besplv/%D0%B2%D0%B8%D1%80%D1%82%D1%83%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5-%D1%81%D0%B5%D1%82%D0%B5%D0%B2%D1%8B%D0%B5-%D1%83%D1%81%D1%82%D1%80%D0%BE%D0%B9%D1%81%D1%82%D0%B2%D0%B0-%D0%B2-linux-linux-bridge-7e0e887edd01
+- https://accedian.com/enterprises/blog/capture-network-traffic-span-vs-tap/
+- https://www.quora.com/What-is-SPAN-RSPAN-in-networks
+- http://etutorials.org/Networking/Lan+switching+first-step/Chapter+11.+Switch+Network+Management/Switch+Port+Analyzer+SPAN/
+- https://wiki.merionet.ru/seti/8/primer-nastrojki-span-rspan-erspan/
+- http://vger.kernel.org/lpc_net2018_talks/erspan-linux-presentation.pdf
+
+
+### GRE
+- https://wiki.dieg.info/gre
+- https://www.ylsoftware.com/news/671
+
+
+### Network traffic capture
+- https://www.rsreese.com/network-traffic-capture-on-linux-using-openvswitch/
+- https://www.flowtraq.com/configuring-netflow-export-open-vswitch-sdn/
+- https://www.plixer.com/blog/ipfix-2/open-vswitch-netflow/
+
+
+### Open vSwitch?
+
+- http://www.openvswitch.org/
+- http://docs.openvswitch.org/en/latest/faq/configuration/
+
+
+### Performance measure
+- www.openvswitch.org/support/ovscon2014/18/1600-ovs_perf.pptx
+- https://developers.redhat.com/blog/2017/06/05/measuring-and-comparing-open-vswitch-performance/
+- http://www.diva-portal.org/smash/get/diva2:1049541/FULLTEXT02
+- https://download.01.org/packet-processing/ONPS1.5/Intel_ONP_Server_Release_1.5_Performance_Test_Report_Rev1.2.pdf
+- https://blog.sflow.com/2015/01/open-vswitch-performance-monitoring.html
